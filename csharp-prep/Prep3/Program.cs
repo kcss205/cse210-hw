8e4658b5282a6df5d35f0505fa8b561aece0157a
kcss205 <23028@byui.edu>
2024-05-01 18:36:31 -0600

    Prep5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 200);
+        int magicNumber = randomGenerator.Next(1, 100);
 
         int guess = -1;
 
