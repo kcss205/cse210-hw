2a684c2f6e851bda32fe47d1ab0585f12c9563f7
kcss205 <23028@byui.edu>
2024-05-06 15:49:50 -0600

    Class

---
+++ prepare/Learning02/Person.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Person {
+
+    private string givenName;
+
+    private string familyName; 
+
+    public Person(string givenName, string familyName) {
+        this.givenName = givenName;
+        this.familyName = familyName;
+    }
+
+    public void EasternStyleName() {
+        Console.WriteLine($"{this.familyName}, {this.givenName}");
+
+    }
+
+    public void WesternStyleName() {
+        Console.WriteLine($"{this.familyName}, {this.givenName}");
+
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Person fred = new Person("Fred", "Flinstone");
+        //fred.givenName = "Fred";
+        //fred.familyName = "Flinstone";
+
+        Person steve = new Person("Steve", "Minecraft");
+        //steve.givenName = "Steve";
+        //steve.familyName = "Minecraft";
+
+        fred.EasternStyleName();
+        steve.WesternStyleName();
     }
 }
