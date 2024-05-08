616fd7856d21e766c6586d856734ad1ae3ebb3cb
kcss205 <23028@byui.edu>
2024-05-08 14:20:19 -0600

    New

---
+++ demos/Name.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Name
+{
+    static void Main(string[] args)
+    {
+        Person fred = new Person("Fred", "Flinstone");
+        //fred.givenName = "Fred";
+        //fred.familyName = "Flinstone";
+
+        Person steve = new Person("Steve", "Minecraft");
+        //steve.givenName = "Steve";
+        //steve.familyName = "Minecraft";
+
+        fred.EasternStyleName();
+        steve.WesternStyleName();
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,15 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Person fred = new Person("Fred", "Flinstone");
-        //fred.givenName = "Fred";
-        //fred.familyName = "Flinstone";
-
-        Person steve = new Person("Steve", "Minecraft");
-        //steve.givenName = "Steve";
-        //steve.familyName = "Minecraft";
-
-        fred.EasternStyleName();
-        steve.WesternStyleName();
+        Console.WriteLine("Hello Learning02 World!");
     }
 }
