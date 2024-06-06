41b10916aecabb5789cbd447fbfc571ee3ccb810
kcss205 <23028@byui.edu>
2024-06-05 23:52:14 -0600

    encapsulation

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment n1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(n1.GetSummary());
+
+        MathAssignment n2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(n2.GetSummary());
+        Console.WriteLine(n2.GetHomeworkList());
+
+        WritingAssignment n3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(n3.GetSummary());
+        Console.WriteLine(n3.GetWritingInformation());
     }
 }
